099b84a925e34b769a904b4e0dc5cd4cba8bf815
Rama <ramiroquesadaperez@gmail.com>
2023-02-08 17:31:01 -0300

    Advances in Inicio

--- src/index.js
+++ src/index.js
@@ -15,14 +15,46 @@ header.appendChild(headerHome);
 
 let headerMenu = document.createElement('div');
 headerMenu.classList.add('headerMenu');
-headerMenu.innerHTML = '<p>Menu<p>'
+headerMenu.innerHTML = '<p>Menú<p>'
 header.appendChild(headerMenu);
 
 let headerAbout = document.createElement('div');
 headerAbout.classList.add('headerAbout');
-headerAbout.innerHTML = '<p>Sobre Nosotros</p>';
+headerAbout.innerHTML = '<p>Contacto</p>';
 header.appendChild(headerAbout)
 
 
 
 container.append(header)
+
+
+
+
+// INDEX CONTENT
+
+let content = document.createElement('div');
+content.classList.add('content');
+content.setAttribute('id', 'content');
+
+content.innerHTML = `<p class="bienvenidos">Bienvenidos</p>
+<p class="matadero">El Matadero</p>
+<button type="button" class="buttonMenu">Ver el Menú</button>`
+
+container.append(content)
+
+
+
+
+
+//  FOOTER
+let footer = document.createElement('footer');
+footer.classList.add('footer');
+footer.setAttribute('id', 'footer')
+footer.innerHTML = `<p>
+  Creado por
+  <a href="https://github.com/ramessj" target="blank">Ramiro Quesada</a>
+  • Hosteado en
+  <a href="https://github.com/ramessj/Restaurant-Page" target="blank">GitHub</a>
+</p>`;
+
+container.append(footer);
