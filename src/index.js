7879b9d3200f523db4bd19732c974135c5d66616
Ramiro Quesada <ramiroquesadaperez@gmail.com>
2023-02-08 12:51:18 -0300

    advances

--- src/index.js
+++ src/index.js
@@ -1,2 +1,28 @@
-let a = 1;
-console.log("Hola")
+let container = document.getElementById("container");
+
+
+
+
+
+let header = document.createElement('div');
+header.classList.add('header');
+
+let headerHome =document.createElement('div');
+headerHome.classList.add('headerHome');
+headerHome.classList.add('headerSelected');
+headerHome.innerHTML = '<p>Inicio</p>'
+header.appendChild(headerHome);
+
+let headerMenu = document.createElement('div');
+headerMenu.classList.add('headerMenu');
+headerMenu.innerHTML = '<p>Menu<p>'
+header.appendChild(headerMenu);
+
+let headerAbout = document.createElement('div');
+headerAbout.classList.add('headerAbout');
+headerAbout.innerHTML = '<p>Sobre Nosotros</p>';
+header.appendChild(headerAbout)
+
+
+
+container.append(header)
