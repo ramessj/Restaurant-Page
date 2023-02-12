e5de3c51715245f4330cd90a93b5bd14af35e359
Rama <ramiroquesadaperez@gmail.com>
2023-02-12 10:22:38 -0300

    Terminado

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,29 @@
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "build.js",
-    path: path.resolve(__dirname, "dist")
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"]
+        use: ["style-loader", "css-loader"],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
-    ]
+    ],
   },
-
-    mode: "production",
-  };
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: "index.html",
+      template: "./dist/index.html",
+    }),
+  ],
+  mode: "production",
+};
